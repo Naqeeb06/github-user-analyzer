a29a9070712bc0c96400b4814bf49f35a8579304
Muhammad Naqeeb Ur Rehman <naqeeburrehman005@gmail.com>
2026-07-14 20:28:02 +0500

    Add input validation and HTTP error handling

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import github_api
+from requests.exceptions import HTTPError
+while True:
+    user_input = input("\nEnter the GitHub username you want to analyze: ")
 
-user_input = input("Enter the GitHub username you want to analyze: ")
-response = github_api.get_user(user_input)
-print(response)
+    if not user_input.strip():
+        print("\nUsername cannot be empty")
+        continue
+    
+    break
+try:
+    response = github_api.get_user(user_input)
+    print(f"\n {response}")
+except HTTPError as e:
+    status_code = e.response.status_code
+    if status_code == 404:
+        print(f"GitHub user '{user_input}' was not found.")
+    elif status_code == 403:
+        print("GitHub API access was denied or the rate limit has been exceeded.Please try again later.")
+    else:
+        print(f"GitHub API returned an error '{status_code}'")
